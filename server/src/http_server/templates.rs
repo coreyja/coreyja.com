8bd5e218b9b8d58c2bbeea9a00c5d5762947cc61
Corey Alexander <coreyja@gmail.com>
2023-03-15 20:14:58 -0400

    Add a simple header for the site with my logo

--- server/src/http_server/templates.rs
+++ server/src/http_server/templates.rs
@@ -1,4 +1,6 @@
-use maud::{html, Markup};
+use maud::{html, Markup, PreEscaped};
+
+const LOGO_SVG: &str = include_str!("../../static/logo.svg");
 
 pub fn head() -> Markup {
     html! {
@@ -9,11 +11,47 @@ pub fn head() -> Markup {
     }
 }
 
+pub fn header() -> Markup {
+    html! {
+      div class="flex px-8" {
+        div class="max-w-md mx-auto p-4 flex-grow" {
+          (PreEscaped(LOGO_SVG))
+        }
+
+        nav class="flex flex-grow justify-end" {
+          ul class="grid grid-flow-col items-center gap-16" {
+            li {
+              a href="/" { "Home" }
+            }
+
+            li {
+              a href="/posts" { "Posts" }
+            }
+
+            li {
+              a href="/projects" { "Projects" }
+            }
+
+            li {
+              a href="/streaming" { "Streaming" }
+            }
+
+            li {
+              a href="/contact" { "Contact" }
+            }
+          }
+        }
+      }
+    }
+}
+
 pub fn base(inner: Markup) -> Markup {
     html! {
       (head())
 
       body class="bg-background text-white" {
+        (header())
+
         (inner)
       }
     }
