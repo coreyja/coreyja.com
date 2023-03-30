c13706e6e3651cf0dd307eb7d4f317dae3f79888
Corey Alexander <coreyja@gmail.com>
2023-03-29 21:14:34 -0400

    Get a little executable written that tells which pages work on the new site

---
+++ sitemap-checker/src/main.rs
@@ -0,0 +1,39 @@
+use miette::{IntoDiagnostic, Result};
+use reqwest::Url;
+use sitemap::{reader::SiteMapReader, structs::UrlEntry};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    println!("Hello, world!");
+    let body = reqwest::get("https://coreyja.com/sitemap.xml")
+        .await
+        .into_diagnostic()?
+        .text()
+        .await
+        .into_diagnostic()?;
+
+    let parser = SiteMapReader::new(body.as_bytes());
+
+    for url in parser {
+        if let sitemap::reader::SiteMapEntity::Url(UrlEntry {
+            loc: sitemap::structs::Location::Url(url),
+            ..
+        }) = url
+        {
+            let mut new_url = url.clone();
+            new_url
+                .set_host(Some("beta.coreyja.com"))
+                .into_diagnostic()?;
+
+            let response = reqwest::get(new_url.clone()).await.into_diagnostic()?;
+
+            if response.status().is_success() {
+                println!("✅: {new_url}");
+            } else {
+                println!("❌: {new_url}");
+            }
+        }
+    }
+
+    Ok(())
+}
