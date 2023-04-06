fbbdab1753cf02a9ed06233641d912c4db85e6e7
Corey Alexander <coreyja@gmail.com>
2023-04-05 20:51:34 -0400

    Support with or without the md extension, and work with a trailing slash

--- server/src/http_server/blog.rs
+++ server/src/http_server/blog.rs
@@ -1,5 +1,5 @@
-use axum::extract::Path;
-use include_dir::{include_dir, Dir, File};
+use axum::{extract::Path, http::StatusCode};
+use include_dir::{include_dir, Dir};
 use markdown::{
     mdast::{Node, Root},
     to_html, to_mdast, ParseOptions,
@@ -9,19 +9,32 @@ use serde::{Deserialize, Serialize};
 
 static BLOG_DIR: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/../blog");
 
-pub async fn post_get(Path(key): Path<String>) -> Markup {
-    let Some(markdown) = BlogPostPath::new(key).to_markdown() else {
-      return html! {
-        h1 { "404" }
-      };
+pub async fn post_get(Path(mut key): Path<String>) -> Result<Markup, StatusCode> {
+    // TODO: Eventually
+    //
+    // I think we can move away from the wildcard route and instead
+    // use the static-ness of BLOG_DIR to setup all the routes on server
+    // boot.
+    // Thay way we can static routes to route the different posts and avoid the wildcard
+    // This might make it easier to do something like generate a sitemap from the routes
+    key = key.strip_suffix('/').unwrap_or(&key).to_string();
+
+    let mut path = BlogPostPath::new(key.clone());
+
+    if !path.file_exists() {
+        path = BlogPostPath::new(format!("{}.md", key));
+    }
+
+    let Some(markdown) = path.to_markdown() else {
+      return Err(StatusCode::NOT_FOUND);
     };
 
-    html! {
+    Ok(html! {
       h1 { (markdown.title) }
       subtitle { (markdown.date) }
 
       (markdown.html)
-    }
+    })
 }
 
 struct BlogPostPath {
@@ -31,6 +44,10 @@ struct BlogPostPath {
 impl BlogPostPath {
     pub fn new(path: String) -> Self {
         Self { path }
+    }
+
+    pub fn file_exists(&self) -> bool {
+        BLOG_DIR.get_file(&self.path).is_some()
     }
 
     pub fn to_markdown(&self) -> Option<PostMarkdown> {
