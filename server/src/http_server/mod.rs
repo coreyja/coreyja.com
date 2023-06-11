5c30c4f373c61ba09c6e803617bcf58732e9ef84
Corey Alexander <coreyja@gmail.com>
2023-06-11 13:56:01 -0400

    Get static asset loading working, and add hero background image

--- server/src/http_server/mod.rs
+++ server/src/http_server/mod.rs
@@ -1,10 +1,11 @@
 use axum::{
-    extract::State,
+    extract::{Path, State},
     http::Uri,
     response::{IntoResponse, Redirect, Response},
     routing::{get, post},
     Router, Server,
 };
+use include_dir::*;
 use std::{net::SocketAddr, sync::Arc};
 use tower_http::trace::TraceLayer;
 
@@ -37,6 +38,8 @@ mod templates;
 
 const TAILWIND_STYLES: &str = include_str!("../../../target/tailwind.css");
 
+const STATIC_ASSETS: Dir<'_> = include_dir!("$CARGO_MANIFEST_DIR/static");
+
 pub(crate) async fn run_axum(config: AppState) -> miette::Result<()> {
     let syntax_css = syntect::html::css_for_theme_with_class_style(
         &config.markdown_to_html_context.theme,
@@ -45,6 +48,7 @@ pub(crate) async fn run_axum(config: AppState) -> miette::Result<()> {
     .unwrap();
 
     let app = Router::new()
+        .route("/static/*path", get(static_assets))
         .route("/styles/syntax.css", get(|| async move { syntax_css }))
         .route("/styles/tailwind.css", get(|| async { TAILWIND_STYLES }))
         .route("/", get(pages::home::home_page))
@@ -98,4 +102,24 @@ async fn fallback(uri: Uri, State(posts): State<Arc<BlogPosts>>) -> Response {
         }
         None => axum::http::StatusCode::NOT_FOUND.into_response(),
     }
+}
+
+async fn static_assets(Path(p): Path<String>) -> impl IntoResponse {
+    let path = p.strip_prefix('/').unwrap_or(&p);
+    let path = path.strip_suffix('/').unwrap_or(path);
+
+    let entry = STATIC_ASSETS
+        .get_file(path)
+        .ok_or_else(|| miette::miette!("Static asset {} not found", path))
+        .unwrap();
+
+    let mime = mime_guess::from_path(path).first_or_octet_stream();
+
+    let mut headers = axum::http::HeaderMap::new();
+    headers.insert(
+        axum::http::header::CONTENT_TYPE,
+        mime.to_string().parse().unwrap(),
+    );
+
+    (headers, entry.contents())
 }
