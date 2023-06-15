7ba0fed99f22c211fbaeb7c17e98d0c2b52dab27
Corey Alexander <coreyja@gmail.com>
2023-06-14 23:46:54 -0400

    Add better mobile viewport metas

--- server/src/http_server/templates/mod.rs
+++ server/src/http_server/templates/mod.rs
@@ -12,6 +12,8 @@ pub fn head() -> Markup {
         link rel="preconnect" href="https://fonts.googleapis.com" {}
         link rel="preconnect" href="https://fonts.gstatic.com" crossorigin {}
         link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&&display=swap" rel="stylesheet" {}
+
+        meta name="viewport" content="width=device-width, initial-scale=1";
       }
     }
 }
