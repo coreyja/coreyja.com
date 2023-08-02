3a1a92f754b28a92f85736c72c3e76c349ca808f
Corey Alexander <coreyja@gmail.com>
2023-08-02 11:12:05 -0400

    Refactor into a cli thats not in server

--- server/src/commands.rs
+++ server/src/commands.rs
@@ -1,18 +1,14 @@
 use clap::Subcommand;
 use miette::Result;
 
-use self::video_backlog::TranscribeVideos;
-
 pub(crate) mod info;
 pub(crate) mod validate;
-pub(crate) mod video_backlog;
 
 #[derive(Subcommand)]
 pub(crate) enum Command {
     Serve,
     Print,
     Validate,
-    TranscribeVideos(TranscribeVideos),
 }
 
 impl Default for Command {
@@ -27,7 +23,6 @@ impl Command {
             Command::Serve => crate::http_server::cmd::serve().await,
             Command::Print => info::print_info().await,
             Command::Validate => validate::validate().await,
-            Command::TranscribeVideos(cmd) => cmd.transcribe_videos().await,
         }
     }
 }
---
+++ video-toolkit/src/main.rs
@@ -0,0 +1,33 @@
+use clap::{Parser, Subcommand};
+use tracing_common::setup_tracing;
+use transcribe::TranscribeVideos;
+
+mod transcribe;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct CliArgs {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand, Debug)]
+enum Command {
+    TranscribeVideos(TranscribeVideos),
+}
+
+#[tokio::main]
+async fn main() -> miette::Result<()> {
+    std::env::set_var("RUST_LOG", "info");
+
+    setup_tracing()?;
+    let cli = CliArgs::parse();
+
+    match cli.command {
+        Command::TranscribeVideos(transcribe_videos) => {
+            transcribe_videos.transcribe_videos().await?
+        }
+    }
+
+    Ok(())
+}
