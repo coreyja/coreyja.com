0864ac75987d973e7583b01d520dc63f1713d60d
Corey Alexander <coreyja@gmail.com>
2023-08-23 20:46:14 -0400

    Setting up byte to be fully functional
    That title was written by the bot, from me asking it aloud!
    So while the actual message isn't great, I think its pretty cool that Byte helped out.
    
    We know can record audio into a buffer and transcribe it at regular intervals.
    If we find the word "Byte" in the transcription we can trigger the bot.
    Right now we send the transcription to OpenAI and use the MacOS 'say' Command
    to hear the response!
    
    From here we can work on our prompt to give Byte a personality.
    We'll likely also want to work on performance so the replies come in quicker
    after I say them

--- byte/src/main.rs
+++ byte/src/main.rs
@@ -1,198 +1,185 @@
-use std::{
-    fs::File,
-    io::BufWriter,
-    path::Path,
-    sync::{Arc, Mutex},
-};
+use std::{process::Command, sync::mpsc::Sender};
 
-use cpal::{
-    traits::{DeviceTrait, HostTrait, StreamTrait},
-    FromSample, Sample,
-};
-use hound::WavReader;
+use cpal::traits::{DeviceTrait, HostTrait, StreamTrait};
 pub use miette::Result;
 use miette::{miette, Context, IntoDiagnostic};
+use openai::{
+    chat::{complete_chat, ChatMessage, ChatRole},
+    OpenAiConfig,
+};
 use tracing_common::setup_tracing;
 use whisper_rs::{FullParams, SamplingStrategy, WhisperContext};
 
 const PREFERRED_MIC_NAME: &str = "Samson G-Track Pro";
 
+fn recording_thread_main(string_sender: Sender<String>) -> Result<()> {
+    loop {
+        let host = cpal::default_host();
+
+        let device = host
+            .input_devices()
+            .into_diagnostic()?
+            .find(|device| match device.name() {
+                Ok(name) => name == PREFERRED_MIC_NAME,
+                Err(err) => {
+                    eprintln!("Failed to get device name: {}", err);
+                    false
+                }
+            })
+            .ok_or_else(|| {
+                miette::miette!("No input device found with name {}", PREFERRED_MIC_NAME)
+            })?;
+
+        println!("Input device: {}", device.name().into_diagnostic()?);
+
+        let config = device
+            .default_input_config()
+            .into_diagnostic()
+            .wrap_err("Failed to get default input config")?;
+        println!("Default input config: {:?}", config);
+
+        println!("Begin recording...");
+
+        let err_fn = move |err| {
+            eprintln!("an error occurred on stream: {}", err);
+        };
+
+        let (sender, receiver) = std::sync::mpsc::channel::<Vec<f32>>();
+
+        let stream = match config.sample_format() {
+            cpal::SampleFormat::F32 => device
+                .build_input_stream(
+                    &config.into(),
+                    move |data, _: &_| sender.send(data.to_vec()).unwrap(),
+                    err_fn,
+                    None,
+                )
+                .into_diagnostic()?,
+            sample_format => {
+                return Err(miette::miette!(
+                    "Unsupported sample format '{sample_format}'"
+                ))
+            }
+        };
+
+        let path_to_model = concat!(env!("CARGO_MANIFEST_DIR"), "/../models/ggml-base.en.bin");
+
+        // load a context and model
+        let ctx = WhisperContext::new(path_to_model).expect("failed to load model");
+
+        stream.play().into_diagnostic()?;
+
+        let mut recorded_sample = vec![];
+        while let Ok(mut data) = receiver.recv() {
+            recorded_sample.append(&mut data);
+
+            if recorded_sample.len() >= 480000 {
+                let resampled = samplerate::convert(
+                    48000,
+                    16000,
+                    2,
+                    samplerate::ConverterType::SincBestQuality,
+                    &recorded_sample,
+                )
+                .unwrap();
+                let pcm_data = convert_stereo_to_mono_audio(&resampled).map_err(|e| miette!(e))?;
+                let mut params = FullParams::new(SamplingStrategy::BeamSearch {
+                    beam_size: 5,
+                    patience: 0.1,
+                });
+                params.set_print_special(false);
+                params.set_print_progress(false);
+                params.set_print_realtime(false);
+                params.set_print_timestamps(false);
+
+                let mut state = ctx.create_state().expect("failed to create state");
+                state
+                    .full(params, &pcm_data[..])
+                    .expect("failed to run model");
+
+                let num_segments = state
+                    .full_n_segments()
+                    .expect("failed to get number of segments");
+                for i in 0..num_segments {
+                    if let Ok(segment) = state.full_get_segment_text(i) {
+                        string_sender.send(segment).unwrap();
+                    }
+                }
+
+                recorded_sample.clear();
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
-    let path_to_model = concat!(env!("CARGO_MANIFEST_DIR"), "/../models/ggml-large.bin");
-
-    // load a context and model
-    let ctx = WhisperContext::new(path_to_model).expect("failed to load model");
-
     setup_tracing()?;
 
-    let host = cpal::default_host();
+    let (sender, reciever) = std::sync::mpsc::channel::<String>();
 
-    let device = host
-        .input_devices()
-        .into_diagnostic()?
-        .find(|device| match device.name() {
-            Ok(name) => name == PREFERRED_MIC_NAME,
-            Err(err) => {
-                eprintln!("Failed to get device name: {}", err);
-                false
+    let _recording = tokio::task::spawn_blocking(move || {
+        let _ = recording_thread_main(sender);
+    });
+
+    let (message_sender, message_reciever) = std::sync::mpsc::channel::<String>();
+
+    let _detecting = tokio::task::spawn_blocking(move || {
+        let mut buffer = vec![];
+        let mut buffering = false;
+        loop {
+            let text = reciever.recv().unwrap();
+            let text = text.to_lowercase();
+            // println!("{}", text);
+
+            if buffering {
+                buffer.push(text);
+                buffering = false;
+
+                message_sender.send(buffer.join(" ")).unwrap();
+                buffer.clear();
+            } else if text.contains("bite") || text.contains("byte") {
+                println!("Bite detected!");
+                buffer.push(text);
+                buffering = true
             }
-        })
-        .ok_or_else(|| miette::miette!("No input device found with name {}", PREFERRED_MIC_NAME))?;
-
-    println!("Input device: {}", device.name().into_diagnostic()?);
-
-    let config = device
-        .default_input_config()
-        .into_diagnostic()
-        .wrap_err("Failed to get default input config")?;
-    println!("Default input config: {:?}", config);
-
-    // The WAV file we're recording to.
-    const PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/tmp/recorded.wav");
-    let spec = wav_spec_from_config(&config);
-    let writer = hound::WavWriter::create(PATH, spec)
-        .into_diagnostic()
-        .wrap_err("Could not make Hound writer")?;
-    let writer = Arc::new(Mutex::new(Some(writer)));
-
-    // A flag to indicate that recording is in progress.
-    println!("Begin recording...");
-
-    // Run the input stream on a separate thread.
-    let writer_2 = writer.clone();
-
-    let err_fn = move |err| {
-        eprintln!("an error occurred on stream: {}", err);
-    };
-
-    let stream = match config.sample_format() {
-        cpal::SampleFormat::I8 => device
-            .build_input_stream(
-                &config.into(),
-                move |data, _: &_| write_input_data::<i8, i8>(data, &writer_2),
-                err_fn,
-                None,
-            )
-            .into_diagnostic()?,
-        cpal::SampleFormat::I16 => device
-            .build_input_stream(
-                &config.into(),
-                move |data, _: &_| write_input_data::<i16, i16>(data, &writer_2),
-                err_fn,
-                None,
-            )
-            .into_diagnostic()?,
-        cpal::SampleFormat::I32 => device
-            .build_input_stream(
-                &config.into(),
-                move |data, _: &_| write_input_data::<i32, i32>(data, &writer_2),
-                err_fn,
-                None,
-            )
-            .into_diagnostic()?,
-        cpal::SampleFormat::F32 => device
-            .build_input_stream(
-                &config.into(),
-                move |data, _: &_| write_input_data::<f32, f32>(data, &writer_2),
-                err_fn,
-                None,
-            )
-            .into_diagnostic()?,
-        sample_format => {
-            return Err(miette::miette!(
-                "Unsupported sample format '{sample_format}'"
-            ))
         }
-    };
+    });
 
-    stream.play().into_diagnostic()?;
+    let openai_config = OpenAiConfig::from_env()?;
+    loop {
+        while let Ok(message) = message_reciever.try_recv() {
+            println!("{}", message);
 
-    // Let recording go for roughly three seconds.
-    std::thread::sleep(std::time::Duration::from_secs(3));
-    drop(stream);
-    writer
-        .lock()
-        .map_err(|_| miette::miette!("Failed to lock writer"))?
-        .take()
-        .ok_or_else(|| miette::miette!("Writer is None"))?
-        .finalize()
-        .into_diagnostic()?;
-    println!("Recording {} complete!", PATH);
-    drop(writer);
+            let messages = vec![
+                ChatMessage {
+                    role: ChatRole::System,
+                    content: r#"
+                The following message was recorded and transcribed during a live chat.
+                I have a bot named Byte (or maybe Bite) that I might be trying to talk to.
+                Remeber this is a AI transcribed audio, so there may be errors in the transcription.
+                
+                You are a helpful chatbot and should respond as Byte.
+                You are my stream companion and can answer any question I ask
+                "#
+                    .to_string(),
+                },
+                ChatMessage {
+                    role: ChatRole::User,
+                    content: message,
+                },
+            ];
+            let resp = complete_chat(&openai_config, "gpt-3.5-turbo", messages).await?;
+            // println!("Response: {}", resp.content);
 
-    {
-        let params = FullParams::new(SamplingStrategy::Greedy { best_of: 1 });
-
-        let path = std::path::PathBuf::from(PATH);
-        let data = parse_wav_file(&path);
-        let resampled = samplerate::convert(
-            48000,
-            16000,
-            2,
-            samplerate::ConverterType::SincBestQuality,
-            &data,
-        )
-        .unwrap();
-        let pcm_data = convert_stereo_to_mono_audio(&resampled).map_err(|e| miette!(e))?;
-
-        let mut state = ctx.create_state().expect("failed to create state");
-        state
-            .full(params, &pcm_data[..])
-            .expect("failed to run model");
-
-        let num_segments = state
-            .full_n_segments()
-            .expect("failed to get number of segments");
-        for i in 0..num_segments {
-            let segment = state
-                .full_get_segment_text(i)
-                .expect("failed to get segment");
-            let start_timestamp = state
-                .full_get_segment_t0(i)
-                .expect("failed to get segment start timestamp");
-            let end_timestamp = state
-                .full_get_segment_t1(i)
-                .expect("failed to get segment end timestamp");
-            println!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
+            Command::new("say")
+                .arg(resp.content)
+                .spawn()
+                .expect("failed to run say");
         }
     }
 
     Ok(())
-}
-
-fn wav_spec_from_config(config: &cpal::SupportedStreamConfig) -> hound::WavSpec {
-    hound::WavSpec {
-        channels: config.channels() as _,
-        sample_rate: config.sample_rate().0 as _,
-        bits_per_sample: (config.sample_format().sample_size() * 8) as _,
-        sample_format: sample_format(config.sample_format()),
-    }
-}
-
-fn sample_format(format: cpal::SampleFormat) -> hound::SampleFormat {
-    if format.is_float() {
-        hound::SampleFormat::Float
-    } else {
-        hound::SampleFormat::Int
-    }
-}
-
-type WavWriterHandle = Arc<Mutex<Option<hound::WavWriter<BufWriter<File>>>>>;
-
-fn write_input_data<T, U>(input: &[T], writer: &WavWriterHandle)
-where
-    T: Sample,
-    U: Sample + hound::Sample + FromSample<T>,
-{
-    if let Ok(mut guard) = writer.try_lock() {
-        if let Some(writer) = guard.as_mut() {
-            for &sample in input.iter() {
-                let sample: U = U::from_sample(sample);
-                writer.write_sample(sample).ok();
-            }
-        }
-    }
 }
 
 pub fn convert_stereo_to_mono_audio(samples: &[f32]) -> Result<Vec<f32>, &'static str> {
@@ -205,34 +192,4 @@ pub fn convert_stereo_to_mono_audio(samples: &[f32]) -> Result<Vec<f32>, &'stati
         .chunks_exact(2)
         .map(|x| (x[0] + x[1]) / 2.0)
         .collect())
-}
-
-fn parse_wav_file(path: &Path) -> Vec<f32> {
-    let reader = WavReader::open(path).expect("failed to read file");
-
-    // if reader.spec().channels != 1 {
-    //     panic!("expected mono audio file");
-    // }
-    // if reader.spec().sample_format != SampleFormat::Int {
-    //     panic!("expected integer sample format");
-    // }
-    // if reader.spec().sample_rate != 16000 {
-    //     panic!("expected 16KHz sample rate");
-    // }
-    // if reader.spec().bits_per_sample != 16 {
-    //     panic!("expected 16 bits per sample");
-    // }
-
-    reader
-        .into_samples::<f32>()
-        .map(|x| x.expect("sample"))
-        .collect::<Vec<_>>()
-}
-
-pub fn convert_integer_to_float_audio(samples: &[i32]) -> Vec<f32> {
-    let mut floats = Vec::with_capacity(samples.len());
-    for sample in samples {
-        floats.push(*sample as f32);
-    }
-    floats
 }
