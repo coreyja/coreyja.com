8dd00e71fdc8a5d4a00b603cc00c58997cff4369
Corey Alexander <coreyja@gmail.com>
2024-01-20 23:51:53 -0500

    :tada: We finally found the magic sauce to get nice errors like we wanted

--- server/src/http_server/errors.rs
+++ server/src/http_server/errors.rs
@@ -1,11 +1,11 @@
-use std::fmt::Display;
+use std::fmt::{Debug, Display, Write};
 
 use axum::http::StatusCode;
 use axum::response::IntoResponse;
 use miette::{Diagnostic, EyreContext, NarratableReportHandler};
 use thiserror::Error;
 
-#[derive(Debug, Diagnostic, Error)]
+#[derive(Diagnostic, Error)]
 pub struct MietteError(pub(crate) miette::Report, pub(crate) StatusCode);
 
 impl Display for MietteError {
@@ -16,11 +16,22 @@ impl Display for MietteError {
     }
 }
 
+impl Debug for MietteError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let handler = NarratableReportHandler::new();
+
+        f.write_fmt(format_args!("Status Code: {}", self.1))?;
+        f.write_str("MietteError: \n")?;
+
+        handler.debug(self.0.as_ref(), f)
+    }
+}
+
 impl IntoResponse for MietteError {
     fn into_response(self) -> axum::response::Response {
         sentry::capture_error(&self);
 
-        tracing::error!(error = %self, "MietteError");
+        tracing::error!(error = ?self, "MietteError");
 
         (self.1, self.0.to_string()).into_response()
     }
