40c2771aa9c032055c3ec7be445fdda0a033ea53
Corey Alexander <coreyja@gmail.com>
2024-01-21 21:24:57 -0500

    Ehh not a type map, but this macro will work

--- cja/src/jobs/registry.rs
+++ cja/src/jobs/registry.rs
@@ -6,3 +6,24 @@ use super::worker::JobFromDB;
 pub trait JobRegistry<AppState: app_state::AppState> {
     async fn run_job(&self, job: &JobFromDB, app_state: AppState) -> miette::Result<()>;
 }
+
+#[macro_export]
+macro_rules! impl_job_registry {
+    ($state:ty, $($job_type:ty),*) => {
+        pub(crate) struct Jobs;
+
+        #[async_trait::async_trait]
+        impl JobRegistry<$state> for Jobs {
+            async fn run_job(&self, job: &JobFromDB, app_state: $state) -> miette::Result<()> {
+                let payload = job.payload.clone();
+
+                match job.name.as_str() {
+                    $(
+                        $job_type::NAME => $job_type::run_from_value(payload, app_state).await,
+                    )*
+                    _ => Err(miette::miette!("Unknown job type: {}", job.name)),
+                }
+            }
+        }
+    };
+}
--- server/src/jobs/mod.rs
+++ server/src/jobs/mod.rs
@@ -1,4 +1,7 @@
-use cja::jobs::{registry::JobRegistry, worker::JobFromDB, Job};
+use cja::{
+    impl_job_registry,
+    jobs::{registry::JobRegistry, worker::JobFromDB, Job},
+};
 
 use crate::state::AppState;
 
@@ -7,17 +10,4 @@ use self::{sponsors::RefreshSponsors, youtube_videos::RefreshVideos};
 pub mod sponsors;
 pub mod youtube_videos;
 
-pub(crate) struct Jobs;
-
-#[async_trait::async_trait]
-impl JobRegistry<AppState> for Jobs {
-    async fn run_job(&self, job: &JobFromDB, app_state: AppState) -> miette::Result<()> {
-        let payload = job.payload.clone();
-
-        match job.name.as_str() {
-            "RefreshSponsors" => RefreshSponsors::run_from_value(payload, app_state).await,
-            "RefreshVideos" => RefreshVideos::run_from_value(payload, app_state).await,
-            _ => Err(miette::miette!("Unknown job type: {}", job.name)),
-        }
-    }
-}
+impl_job_registry!(AppState, RefreshSponsors, RefreshVideos);
