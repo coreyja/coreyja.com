e03af1b4e33f890af46aa5fcd5e70551904ed895
Corey Alexander <coreyja@gmail.com>
2024-01-21 23:31:15 -0500

    Create nice interface to register crons too

--- cja/src/cron/registry.rs
+++ cja/src/cron/registry.rs
@@ -10,15 +10,47 @@ pub struct CronRegistry<AppState: AS> {
     pub(super) jobs: HashMap<&'static str, CronJob<AppState>>,
 }
 
-#[allow(clippy::type_complexity)]
-pub(super) struct CronJob<AppState: AS> {
-    name: &'static str,
-    func: Box<
-        dyn Fn(AppState, String) -> Pin<Box<dyn Future<Output = Result<(), String>> + Send>>
+#[async_trait::async_trait]
+pub trait CronFn<AppState: AS> {
+    // This collapses the error type to a string, right now thats because thats
+    // what the only consumer really needs. As we add more error debugging we'll
+    // need to change this.
+    async fn run(&self, app_state: AppState, context: String) -> Result<(), String>;
+}
+
+pub struct CronFnClosure<
+    AppState: AS,
+    FnError: Diagnostic + Send + Sync + 'static,
+    F: Fn(AppState, String) -> Pin<Box<dyn Future<Output = Result<(), FnError>> + Send>>
+        + Send
+        + Sync
+        + 'static,
+> {
+    pub(super) func: F,
+    _marker: std::marker::PhantomData<AppState>,
+}
+
+#[async_trait::async_trait]
+impl<
+        AppState: AS,
+        FnError: Diagnostic + Send + Sync + 'static,
+        F: Fn(AppState, String) -> Pin<Box<dyn Future<Output = Result<(), FnError>> + Send>>
             + Send
             + Sync
             + 'static,
-    >,
+    > CronFn<AppState> for CronFnClosure<AppState, FnError, F>
+{
+    async fn run(&self, app_state: AppState, context: String) -> Result<(), String> {
+        (self.func)(app_state, context)
+            .await
+            .map_err(|err| format!("{:?}", err))
+    }
+}
+
+#[allow(clippy::type_complexity)]
+pub(super) struct CronJob<AppState: AS> {
+    name: &'static str,
+    func: Box<dyn CronFn<AppState> + Send + Sync + 'static>,
     interval: Duration,
 }
 
@@ -54,14 +86,16 @@ impl<AppState: AS> CronJob<AppState> {
                     time_since_last_run =? elapsed,
                     "Enqueuing Task"
                 );
-                (self.func)(app_state, context)
+                (self.func)
+                    .run(app_state, context)
                     .await
                     .map_err(TickError::JobError)?;
                 last_enqueue_map.insert(self.name, Instant::now());
             }
         } else {
             tracing::info!(task_name = self.name, "Enqueuing Task for first time");
-            (self.func)(app_state, context)
+            (self.func)
+                .run(app_state, context)
                 .await
                 .map_err(TickError::JobError)?;
             last_enqueue_map.insert(self.name, Instant::now());
@@ -79,18 +113,21 @@ impl<AppState: AS> CronRegistry<AppState> {
     }
 
     #[tracing::instrument(name = "cron.register", skip_all, fields(cron_job.name = name, cron_job.interval = ?interval))]
-    pub fn register(
+    pub fn register<FnError: Diagnostic + Send + Sync + 'static>(
         &mut self,
         name: &'static str,
         interval: Duration,
-        job: impl Fn(AppState, String) -> Pin<Box<dyn Future<Output = Result<(), String>> + Send>>
+        job: impl Fn(AppState, String) -> Pin<Box<dyn Future<Output = Result<(), FnError>> + Send>>
             + Send
             + Sync
             + 'static,
     ) {
         let cron_job = CronJob {
             name,
-            func: Box::new(job),
+            func: Box::new(CronFnClosure {
+                func: job,
+                _marker: std::marker::PhantomData,
+            }),
             interval,
         };
         self.jobs.insert(name, cron_job);
@@ -99,12 +136,7 @@ impl<AppState: AS> CronRegistry<AppState> {
     #[tracing::instrument(name = "cron.register_job", skip_all, fields(cron_job.name = J::NAME, cron_job.interval = ?interval))]
     pub fn register_job<J: Job<AppState>>(&mut self, job: J, interval: Duration) {
         self.register(J::NAME, interval, move |app_state, context| {
-            let job = job.clone();
-            Box::pin(async move {
-                J::enqueue(job, app_state, context)
-                    .await
-                    .map_err(|_| "Failed to enqueue job".to_string())
-            })
+            J::enqueue(job.clone(), app_state, context)
         });
     }
 }
