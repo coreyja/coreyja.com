b193ed10fbe6fef6dee56aca82520414eb6aed65
Corey Alexander <coreyja@gmail.com>
2024-02-10 21:55:36 -0700

    Add a failing spec to test this quick

--- leptos_site/end2end/tests/example.spec.ts
+++ leptos_site/end2end/tests/example.spec.ts
@@ -7,3 +7,11 @@ test("homepage has title", async ({ page }) => {
 
   await expect(page.locator("h1")).toHaveText("Welcome to Leptos!");
 });
+
+test("homepage mentions my username", async ({ page }) => {
+  await page.goto("http://localhost:3000/");
+
+  await expect(page).toHaveTitle("Welcome to Leptos");
+
+  await expect(page.locator("html")).toHaveText("coreyja");
+});
