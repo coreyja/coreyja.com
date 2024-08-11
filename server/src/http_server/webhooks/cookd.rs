5bacb27c6578ee662d13a0fa8e327ffc3d7e82d4
Corey Alexander <coreyja@gmail.com>
2024-08-11 15:43:04 -0400

    Cookd Webhooks (#71)
    * First Draft of Webhooks
    
    * Transform to camelCase
    
    * Fix Rust errors and sqlx prepare
    
    * Timestamps
    
    * Add a default to the column

---
+++ server/src/http_server/webhooks/cookd.rs
@@ -0,0 +1,48 @@
+use axum::{extract::State, http::StatusCode, response::IntoResponse, Json};
+use cja::{app_state::AppState as _, color_eyre::eyre::Context};
+use serde_json::{json, Value};
+
+use crate::{
+    http_server::{errors::WithStatus as _, ResponseResult},
+    AppState,
+};
+
+#[derive(serde::Deserialize, Clone)]
+#[serde(rename_all = "camelCase")]
+struct Payload {
+    subdomain: String,
+    slug: String,
+    player_github_email: Option<String>,
+    player_github_username: Option<String>,
+    score: i32,
+}
+
+#[axum_macros::debug_handler]
+pub(crate) async fn handler(
+    State(state): State<AppState>,
+    Json(webhook_payload): Json<Value>,
+) -> ResponseResult<impl IntoResponse> {
+    let payload: Payload = serde_json::from_value(webhook_payload)
+        .context("Could not parse payload into expected JSON")
+        .with_status(StatusCode::UNPROCESSABLE_ENTITY)?;
+
+    let now = chrono::Utc::now();
+
+    let db_result = sqlx::query!(
+        r#"
+        INSERT INTO CookdWebhooks (subdomain, slug, player_github_email, player_github_username, score, created_at, updated_at)
+        VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING cookd_webhook_id
+        "#,
+        payload.subdomain,
+        payload.slug,
+        payload.player_github_email,
+        payload.player_github_username,
+        payload.score,
+        now,
+        now
+    ).fetch_one(state.db()).await.context("Could not insert webhook payload into database")?;
+
+    Ok(Json(
+        json!({ "cookd_webhook_id": db_result.cookd_webhook_id }),
+    ))
+}
