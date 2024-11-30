988a931c5bb1c01dd46226c2d459fc6d7d6780e5
Corey Alexander <coreyja@gmail.com>
2024-11-30 16:08:37 -0500

    Clippy

--- server/src/bsky/types.rs
+++ server/src/bsky/types.rs
@@ -1,8 +1,6 @@
-use rsky_lexicon::app::bsky::feed::PostView;
 use rsky_lexicon::app::bsky::feed::ThreadViewPostEnum;
 use serde::Deserialize;
 use serde::Serialize;
-use serde_json::Value;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
