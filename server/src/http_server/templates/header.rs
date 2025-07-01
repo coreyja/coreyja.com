819179741ede844d11ba1d20e6b35960669c1f23
Corey Alexander <coreyja@gmail.com>
2025-07-01 08:07:41 -0400

    Fix tooltip by only updating on hover
    - Changed tooltip to only update when user hovers over the button
    - Prevents browser from resetting tooltip display due to constant updates
    - Countdown still tracks internally but tooltip text is generated on-demand
    - Ensures tooltip displays properly when user hovers

--- server/src/http_server/templates/header.rs
+++ server/src/http_server/templates/header.rs
@@ -52,6 +52,9 @@ pub fn head(og: impl Borrow<OpenGraph>) -> Markup {
         link rel="stylesheet" href="/styles/syntax.css" {}
         link rel="stylesheet" href="/styles/comic_code.css" {}
 
+        link rel="stylesheet" href="/static/view-transitions.css" {}
+        script src="/static/view-transitions.js" defer {}
+
         link rel="preconnect" href="https://fonts.googleapis.com" {}
         link rel="preconnect" href="https://fonts.gstatic.com" crossorigin {}
         link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&&display=block" rel="stylesheet" {}
